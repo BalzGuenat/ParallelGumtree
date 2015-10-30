095fbec291a2e4e1f74ed195d149b04c72c00565
amirreza <amirreza.bahreini@epfl.ch>
2015-10-30 14:29:05 +0100

    Matcher abstract class partially implemented

--- matcher.cpp
+++ matcher.cpp
@@ -1,7 +1,73 @@
 #include "matcher.h"
+#include <algorithm>    // std::max
 
-Matcher::Matcher()
+Matcher::~Matcher()
 {
 
 }
 
+Mathcher::Matcher(Tree* src, Tree* dst, MappingStore* store)
+{
+    src = src;
+    dst = dst;
+    mappings = store;
+}
+
+MappingStore Matcher::get_mappings()
+{
+    return mappings;
+}
+
+Set<Mapping> Matcher::get_mapping_set()
+{
+    return mappings.asSet();
+}
+
+Tree* Matcher::get_src()
+{
+    return src;
+}
+
+Tree* Matcher::get_dst()
+{
+    return dst;
+}
+
+void Matcher::add_mapping(Tree* src, Tree* dst)
+{
+    //src.setMatched(true);
+    //dst.setMatched(true);
+    mappings.link(src, dst);
+}
+
+void Matcher::add_full_mapping(Tree* src, Tree* dst)
+{
+    //To do
+}
+
+double Matcher::chawatheSimilarity(Tree* src, Tree* dst) {
+    int max = std::max(src->ge_descendants().size(), dst->get_descendants().size());
+    return (double) numberOfCommonDescendants(src, dst) / (double) max;
+}
+
+double Matcher::diceSimilarity(Tree* src, Tree* dst) {
+    double c = (double) numberOfCommonDescendants(src, dst);
+    // WHAT THE ? What is 2D ?
+    return (2D * c) / ((double) src->get_descendants().size() + (double) dst.get_descendants().size());
+}
+
+double Matcher::jaccardSimilarity(Tree* src, Tree* dst) {
+    double num = (double) numberOfCommonDescendants(src, dst);
+    double den = (double) src->get_descendants().size() + (double) dst->get_descendants().size() - num;
+    return num / den;
+}
+
+int Matcher::numberOfCommonDescendants(Tree* src, Tree* dst)
+{
+    //TODO
+}
+
+void Matcher::clean()
+{
+    //TODO
+}
--- matcher.h
+++ matcher.h
@@ -1,17 +1,33 @@
 #ifndef MATCHER_H
 #define MATCHER_H
 
-#include <map>
-#include "tree.h"
-
-using namespace std;
-
-using matching = map<Tree*, Tree*>;
+#include <mappingstore.h>
 
 class Matcher
 {
+
+protected:
+  const Tree* src;
+  const Tree* dst;
+  const MappingStore* mappings;
+
+  void add_mapping(Tree* src, Tree* dst);
+  void add_full_mapping(Tree* src, Tree* dst);
+  double chawatheSimilarity(Tree* src, Tree* dst);
+  double diceSimilarity(Tree* src, Tree* dst);
+  double jaccardSimilarity(Tree* src, Tree* dst);
+  int numberOfCommonDescendants(Tree* src, Tree* dst);
+  void clean();
+
 public:
-  Matcher();
+  Matcher(Tree* src, Tree* dst, MappingStore* store);
+  virtual void match() = 0;
+  MappingStore get_mappings();
+  Set<Mapping> get_mapping_set();
+  Tree* get_src();
+  Tree* get_dst();
+
+  ~Matcher();
 };
 
 #endif // MATCHER_H
