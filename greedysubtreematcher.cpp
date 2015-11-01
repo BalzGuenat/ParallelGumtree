6427048650587762fc0d44bcc80653db57d80611
amirreza <amirreza.bahreini@hotmail.com>
2015-11-01 15:25:17 +0100

    comparator implemented.

--- greedysubtreematcher.cpp
+++ greedysubtreematcher.cpp
@@ -45,8 +45,60 @@ void GreedySubTreeMatcher::filterMappings(MultiMappingStore *multiMappings) {
 }
 
 struct GreedySubTreeMatcher::comparator {
+
+  unordered_map<Mapping*, double> simMap;
+  unordered_map<Tree*, vector<Tree*>> srcDescendants;
+  unordered_map<Tree*, set<Tree*>> dstDescendants;
+
   bool operator() (Mapping &i,Mapping &j)
   {
-
+      return simMap[i] < simMap[j];
   }
-} compare;
+
+  MappingComparator(vector<Mapping*>& mappings)
+  {
+    for (vector<Mapping>::iterator it = mappings.begin(); it != mappings.end(); it++)
+      {
+        simMap.insert(*it, sim((*it)->first, (*it)->second));
+
+      }
+  }
+
+  int numberOfCommonDescendants(Tree* src, Tree* dst) {
+    if (srcDescendants.find(src) == srcDescendants.end())
+        srcDescendants.insert(src, src->children());
+    if (dstDescendants.find(dst) == dstDescendants.end())
+        dstDescendants.insert(dst, unoredered_map(dst->children().begin(), dst->children().end()));
+
+    int common = 0;
+
+    for (set<Tree*>::iterator it = srcDescendants[src].begin(); it != srcDescendants.end(); it++) {
+        Tree m = mappings.getDst(*it);
+        if (m != null && dstDescendants[&dst].find(m) != dstDescendants[&dst].end())
+            common++;
+    }
+
+    return common;
+  }
+
+  double sim(Tree* src, Tree* dst) {
+    double jaccard = jaccardSimilarity(src.getParent(), dst.getParent());
+    int posSrc = (src.isRoot()) ? 0 : src.getParent().getChildPosition(src);
+    int posDst = (dst.isRoot()) ? 0 : dst.getParent().getChildPosition(dst);
+    int maxSrcPos =  (src.isRoot()) ? 1 : src.getParent().getChildren().size();
+    int maxDstPos =  (dst.isRoot()) ? 1 : dst.getParent().getChildren().size();
+    int maxPosDiff = max(maxSrcPos, maxDstPos);
+    //TODO
+//    double pos = 1D - ((double) Math.abs(posSrc - posDst) / (double) maxPosDiff);
+//    double po = 1D - ((double) Math.abs(src.getId() - dst.getId())
+//            / (double) GreedySubtreeMatcher.this.getMaxTreeSize());
+    return 100 * jaccard + 10 * pos + po;
+  }
+
+  double jaccardSimilarity(Tree* src, Tree* dst) {
+    double num = (double) numberOfCommonDescendants(src, dst);
+    double den = (double) srcDescendants[src].size() + (double) dstDescendants[dst].size() - num;
+    return num / den;
+  }
+
+};
