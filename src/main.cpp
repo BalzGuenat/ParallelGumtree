5f02f6b751102e3e31ba8c999d92f6b3247aafb1
Balz Guenat <balz.guenat@gmail.com>
2015-12-09 20:22:58 +0100

    update command line option handling. add -num-threads option

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <omp.h>
 #include "tree.h"
 #include "fileparser.h"
 #include "classicgumtree.h"
@@ -11,19 +12,19 @@ using namespace std;
 
 const int DEFAULT_PRUFERTREE_SIZE = 1000;
 const string USAGE_TEXT = "ParallelGumtree\n"
-								  "Usage: ParallelGumtree FILE0 [FILE1]\n"
-								  "       ParallelGumtree -create [NUMBER]\n"
+								  "Usage: ParallelGumtree [options] [FILE0 FILE1]\n"
 								  "\n"
 								  "where FILEn encodes a tree Tn.\n"
-								  "If FILE1 is provided then a matching between T0 and T1 "
+								  "If FILE0 and FILE1 is provided then a matching between T0 and T1 "
 								  "is computed and printed to stdout.\n"
-								  "If FILE1 is not provided then FILE0 is parsed, T0 is built "
-								  "internally and then written to stdout and \"FILE1.replica\".\n"
+								  "Options:\n"
+								  "\t-create [SIZE]\n"
 								  "If ParallelGumtree is called with the -create option, "
 								  "pruferTrees is called which creates two similar trees "
-								  "in the working directory \ncalled randomPrufer1 and randomPrufer2. "
-								  "If NUMBER is provided (and NUMBER>=10) for -create, the created trees will have about that\n"
-								  "many nodes. Otherwise it will have the default size of about " + to_string(DEFAULT_PRUFERTREE_SIZE) + " nodes.";
+								  "in the working directory \ncalled randomPrufer1 and randomPrufer2"
+								  "with a node count near SIZE.\n"
+								  "\t-num-threads N\n"
+								  "ParallelGumtree will be executed using N threads.";
 
 int main(int argc, char* argv[])
 {
@@ -32,59 +33,78 @@ int main(int argc, char* argv[])
 	for (int i = 0; i < argc; ++i)
 		args[i] = argv[i];
 
+	map<string, int> options;
+
 	if (argc < 2) {
 		cout << USAGE_TEXT << endl;
 		return 0;
 
-	} else if (argc == 2 && args[1] == "-create") {
-		TreeCreator::pruferTrees(DEFAULT_PRUFERTREE_SIZE, "randomPrufer");
-		return 0;
+	}
 
-	} else if (argc == 2) {
-		// one file was passed, replicate it
-		auto file0 = args[1];
-		auto t = FileParser::parse(file0);
-		cout << t->subTreeToString() << endl;
-		FileWriter::write(t, file0 + ".replica");
-		cout << "PostOrder:" << endl;
-		for (auto tt : t->postOrder())
-			cout << tt->toString() << endl;
-		delete t; t = nullptr;
-		return 0;
+	for (int i = 1; i < argc; ++i) {
+		auto arg = args[i];
+		if (arg[0] == '-')
+			options.insert(make_pair(arg,i));
+	}
 
-	} else if (argc == 3 && args[1] == "-create") {
+	map<string, int>::iterator idx_it;
+
+	if ((idx_it = options.find("-create")) != options.end()) {
+		int i = idx_it->second;
+		bool size_provided = false;
 		int size;
-		try {
-			size = stoi(args[2], NULL, 10); // convert
+		if (i+1 < argc && args[i+1][0] != '-')
+			try {
+			size = stoi(args[i+1]);
+			size_provided = true;
 		} catch (const invalid_argument&) {
 			size = 0;
-		} if (size<20) {
-			cout << "The argument NUMBER is smaller than 20 or not an integer. The default size of " << DEFAULT_PRUFERTREE_SIZE << " was taken to create the tree." << endl;
+		}
+		if (size < 20) {
+			if (size_provided)
+				cout << "The argument NUMBER is smaller than 20 or not an integer. The default size of " << DEFAULT_PRUFERTREE_SIZE << " was taken to create the tree." << endl;
 			TreeCreator::pruferTrees(DEFAULT_PRUFERTREE_SIZE, "randomPrufer");
-		  } else {
+		} else {
 			TreeCreator::pruferTrees(size, "randomPrufer");
-		  }
-		return 0;
-	} else if (argc == 3) {
-		// two files were passed, match them
-		auto file0 = args[1];
-		auto file1 = args[2];
-		auto t0 = FileParser::parse(file0);
-		auto t1 = FileParser::parse(file1);
-		// TODO match trees.
-		MappingStore mapping;
-		ClassicGumtree matcher(t0, t1, &mapping);
-		matcher.match();
-		ofstream outputFile(file1 + "_parallel.mtch");
-		for (auto m : mapping.asSet()) {
-			//cout << m.first->lineNumber() << " -> " << m.second->lineNumber() << endl;
-			outputFile << m.first->lineNumber() << " -> " << m.second->lineNumber() << endl;
-		  }
-		outputFile.close();
-		delete t0; t0 = nullptr;
-		delete t1; t1 = nullptr;
-		return 0;
+		}
 	}
-	return -1;
+
+	if ((idx_it = options.find("-num-threads")) != options.end()) {
+		int i = idx_it->second;
+		int num_threads;
+		if (i+1 < argc)
+			try {
+			num_threads = stoi(args[i+1]);
+			omp_set_num_threads(num_threads);
+		} catch (const invalid_argument&) {
+			cout << "No valid argument to option -num-threads provided." << endl;
+			return -1;
+		} else {
+			cout << "No valid argument to option -num-threads provided." << endl;
+			return -1;
+		}
+	}
+
+	if (argc >= 3) {
+		auto file0 = args[argc-2];
+		auto file1 = args[argc-1];
+		if (file0[0] != '-' && file1[0] != '-') {
+			auto t0 = FileParser::parse(file0);
+			auto t1 = FileParser::parse(file1);
+			MappingStore mapping;
+			ClassicGumtree matcher(t0, t1, &mapping);
+			matcher.match();
+			ofstream outputFile(file1 + "_parallel.mtch");
+			for (auto m : mapping.asSet()) {
+				//cout << m.first->lineNumber() << " -> " << m.second->lineNumber() << endl;
+				outputFile << m.first->lineNumber() << " -> " << m.second->lineNumber() << endl;
+			  }
+			outputFile.close();
+			delete t0; t0 = nullptr;
+			delete t1; t1 = nullptr;
+		}
+	}
+
+	return 0;
 }
 
