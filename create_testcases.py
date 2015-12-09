d5f0e0a28b340d87b541cc8b4bbf6468eef29f0d
Balz Guenat <balz.guenat@gmail.com>
2015-12-09 20:16:57 +0100

    fix and clean up python scripts

--- create_testcases.py
+++ create_testcases.py
@@ -4,22 +4,18 @@ import subprocess
 import time
 import os
 
-
-steps = 1000
-value = 1000
-i = 0
 # create test case dir
 if not os.path.exists('test_cases'):
 	os.makedirs('test_cases')
 
 # open and delete the average linecount file's content
-linecount_file = open('linecount', 'w+')
+linecount_file = open('test_cases/linecount', 'w+')
 linecount_file.seek(0)
 linecount_file.truncate()
-while i <= 32000: # create the test case files and write the linecount
+for i in range(0, 8): # create the test case files and write the linecount
 	filename1 = 'test_cases/tree_' + str(i) + '_1.test'
 	filename2 = 'test_cases/tree_' + str(i) + '_2.test'
-	subprocess.call(['./ParallelGumtree', '-create', str(value)])
+	subprocess.call(['./ParallelGumtree', '-create', str((i+1) * 4000)])
 	
 	# move the files
 	os.rename("randomPrufer1", filename1)
@@ -28,7 +24,5 @@ while i <= 32000: # create the test case files and write the linecount
 	# print average linecount to file tree_linecount
 	average_linecount = ((sum(1 for line in open(filename1)) + sum(1 for line in open(filename2)))/2)
 	linecount_file.write(str(average_linecount)+'\n')
-	value+=steps
-	i+=1
 linecount_file.close
-print "Number of test files: " + str(i)
+print "Number of test files: " + str(i+1)
--- run_script.py
+++ run_script.py
@@ -7,39 +7,43 @@ import numpy as np
 import os
 import filecmp
 
-input_range = range(0, 32)
+sizes = 4 #go up to 8 for larger tests
+runs = 4
 
 # empty the average linecount file
 with open('test_cases/linecount') as file:
-	average_linecount = f.readlines()
+	average_linecount = file.readlines()
 average_linecount = map(str.strip, average_linecount)
 average_linecount = map(int, average_linecount)
-
-runs = (0,3)
+del average_linecount[sizes:]
 
 # initialize the arrays for the time
-parallelGumtreeTimes = np.empty([len(runs),len(input_range)])
-referenceGumtreeTimes = np.empty([len(runs),len(input_range)])
-for j in runs:
-	for i in input_range:
-		filename1 = 'test_cases/tree_' + str(i) + '_1.test'
-		filename2 = 'test_cases/tree_' + str(i) + '_2.test'
+parallelGumtreeTimes = np.empty([runs, sizes])
+referenceGumtreeTimes = np.empty([runs, sizes])
+for i in range(0,sizes):
+	filename1 = 'test_cases/tree_' + str(i) + '_1.test'
+	filename2 = 'test_cases/tree_' + str(i) + '_2.test'
 
+	for r in range(0,runs):
 		# run and time the parallel Gumtree
+		print('ParallelGumtree, run ' + str(r) + ' on file ' + str(i))
 		tic = time.time()
 		subprocess.call(['./ParallelGumtree',filename1, filename2])
 		toc = time.time()
-		parallelGumtreeTimes[j][i] = (toc-tic)
+		parallelGumtreeTimes[r][i] = (toc-tic)
+		print('Time: ' + str(toc-tic))
 		
 		# run and time the java reference algorithm
+		print('JavaGumtree, run ' + str(r) + ' on file ' + str(i))
 		tic = time.time()
 		subprocess.call(['java', '-cp', './gumtree.jar', 'com.github.gumtreediff.client.Run', '-c', 'Clients.experimental', 'true', '-c', 'match.gt.minh', '1', '-c', 'match.bu.sim', '0.5', 'testdump', '-g', 'testgen', '-m', 'gumtree', filename1, filename2])
 		toc = time.time()
-		referenceGumtreeTimes[j][i] = (toc - tic)
-		
-		# compare if each solution is the same:
-		if not filecmp.cmp(filename2 + '.mtch', filename2 + '_parallel.mtch'):
-			print "Matches of run " + str(i) + " are not equal!"
+		referenceGumtreeTimes[r][i] = (toc - tic)
+		print('Time: ' + str(toc-tic))
+	
+	# compare if each solution is the same:
+	if not filecmp.cmp(filename2 + '.mtch', filename2 + '_parallel.mtch'):
+		print "Matches of run " + str(i) + " are not equal!"
 # calculate average for the times
 parallel_average_times = parallelGumtreeTimes.mean(axis=0)
 reference_average_times = referenceGumtreeTimes.mean(axis=0)
