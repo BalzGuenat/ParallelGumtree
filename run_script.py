792f7d947960e63a222644cae87375ed57553658
Balz Guenat <balz.guenat@gmail.com>
2015-12-11 03:00:07 +0100

    update run_script for execution with multiple threads

--- run_script.py
+++ run_script.py
@@ -7,8 +7,9 @@ import numpy as np
 import os
 import filecmp
 
-sizes = 4 #go up to 8 for larger tests
+sizes = 6 #go up to 8 for larger tests
 runs = 4
+max_threads = 2
 
 # empty the average linecount file
 with open('test_cases/linecount') as file:
@@ -18,21 +19,22 @@ average_linecount = map(int, average_linecount)
 del average_linecount[sizes:]
 
 # initialize the arrays for the time
-parallelGumtreeTimes = np.empty([runs, sizes])
+parallelGumtreeTimes = np.empty([max_threads, runs, sizes])
 referenceGumtreeTimes = np.empty([runs, sizes])
 for i in range(0,sizes):
 	filename1 = 'test_cases/tree_' + str(i) + '_1.test'
 	filename2 = 'test_cases/tree_' + str(i) + '_2.test'
 
 	for r in range(0,runs):
-		# run and time the parallel Gumtree
-		print('ParallelGumtree, run ' + str(r) + ' on file ' + str(i))
-		tic = time.time()
-		subprocess.call(['./ParallelGumtree',filename1, filename2])
-		toc = time.time()
-		parallelGumtreeTimes[r][i] = (toc-tic)
-		print('Time: ' + str(toc-tic))
-		
+		for num_threads in range(0,max_threads):
+			# run and time the parallel Gumtree
+			print('ParallelGumtree, run ' + str(r+1) + ' on file ' + str(i) + ' with ' + str(2**num_threads) + ' threads')
+			tic = time.time()
+			subprocess.call(['./ParallelGumtree', '-num-threads', str(2**num_threads), filename1, filename2])
+			toc = time.time()
+			parallelGumtreeTimes[num_threads][r][i] = (toc-tic)
+			print('Time: ' + str(toc-tic))
+			
 		# run and time the java reference algorithm
 		print('JavaGumtree, run ' + str(r) + ' on file ' + str(i))
 		tic = time.time()
@@ -44,14 +46,18 @@ for i in range(0,sizes):
 	# compare if each solution is the same:
 	if not filecmp.cmp(filename2 + '.mtch', filename2 + '_parallel.mtch'):
 		print "Matches of run " + str(i) + " are not equal!"
-# calculate average for the times
-parallel_average_times = parallelGumtreeTimes.mean(axis=0)
+# calculate average for the times and plot everything
+for t in range(0,max_threads):
+	parallel_average_times = parallelGumtreeTimes[t].mean(axis=0)
+	plt.plot(average_linecount, parallel_average_times, marker='o')
 reference_average_times = referenceGumtreeTimes.mean(axis=0)
-# plot everything
-plt.plot(average_linecount, parallel_average_times, marker='o')
 plt.plot(average_linecount, reference_average_times, marker='o')
 plt.ylabel('elapsed time')
 plt.xlabel('input size')
-plt.legend(['parallel Gumtree', 'java reference algorithm'], loc='upper left')
+legend = []
+for t in range(0,max_threads):
+	legend.append(str(2**t) + 'threads')
+legend.append('java reference algorithm')
+plt.legend(legend, loc='upper left')
 plt.savefig('aaaplot.png')
 plt.show()
